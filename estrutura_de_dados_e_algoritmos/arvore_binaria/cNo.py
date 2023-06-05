01e319ecdc553edf6f6b7dd800733dabe7f06ec4
João Zeba <joaog.zeba@gmail.com>
2023-06-04 21:09:42 -0300

    Getters e setters criados.

--- estrutura_de_dados_e_algoritmos/arvore_binaria/cNo.py
+++ estrutura_de_dados_e_algoritmos/arvore_binaria/cNo.py
@@ -5,4 +5,30 @@ class No:
         self.left = leftChild
         self.right = rightChild
         
+    def getKey(self):
+        return self.key
+    
+    def setKey(self, value):
+        self.key = value
+        
+    def getFather(self):
+        return self.father
+    
+    def setFather(self, node):
+        self.father = node
+        
+    def getLeftChild(self):
+        return self.left
+    
+    def setLeftChild(self, node):
+        self.left = node
+        
+    def getRightChild(self):
+        return self.right
+    
+    def setRIghtChild(self, node):
+        self.right = node
+        
+    def show(self):
+        return f"{self.getKey}"
     
