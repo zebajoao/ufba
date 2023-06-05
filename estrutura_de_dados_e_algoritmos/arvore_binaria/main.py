a47c1ddfdb5a02d64f6576a3debf219435e1d296
João Zeba <joaog.zeba@gmail.com>
2023-06-04 21:37:18 -0300

    Desenvolvido teste de inserção e impressão.

--- estrutura_de_dados_e_algoritmos/arvore_binaria/main.py
+++ estrutura_de_dados_e_algoritmos/arvore_binaria/main.py
@@ -1,7 +1,24 @@
 from cArvoreBinaria import *
 
 def main():
-    print("Testing binary tree...")
+    print("Testing binary tree...\n")
+    arvore1 = ArvoreBinaria()
+    arvore1.insert(10)
+    arvore1.insert(15)
+    arvore1.insert(5)
+    print("Imprimindo em ordem:\n")
+    arvore1.displayInOrder(arvore1.getRaiz())
+    print()
+    arvore1.insert(13)
+    arvore1.insert(7)
+    arvore1.insert(4)
+    arvore1.insert(20)
+    arvore1.displayInOrder(arvore1.getRaiz())
+    print()
+    '''
+    arvore1.remove(10)
+    arvore1.displayInOrder(arvore1.getRaiz())
+    '''
     
 if __name__ == "__main__":
     main()
