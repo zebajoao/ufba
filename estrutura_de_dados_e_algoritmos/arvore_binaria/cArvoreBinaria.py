3e1c682d0cc1ef372404fe2cb332b0be156b4d61
João Zeba <joaog.zeba@gmail.com>
2023-06-04 21:13:30 -0300

    Adicionado getter e setter.

--- estrutura_de_dados_e_algoritmos/arvore_binaria/cArvoreBinaria.py
+++ estrutura_de_dados_e_algoritmos/arvore_binaria/cArvoreBinaria.py
@@ -3,3 +3,12 @@ from cNo import *
 class ArvoreBinaria:
     def __init__(self):
         self.root = None
+        
+    def getRoot(self):
+        return self.root
+    
+    def setRoot(self, node):
+        self.root = node
+        
+    
+    
