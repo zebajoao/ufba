892c5ef293816c365a9758934dd569f001704675
João Zeba <joaog.zeba@gmail.com>
2023-06-09 23:04:23 -0300

    Função display() adicionada na classe e main() finalizado.

--- estrutura_de_dados_e_algoritmos/pratica_avaliativa_1/round_robin.py
+++ estrutura_de_dados_e_algoritmos/pratica_avaliativa_1/round_robin.py
@@ -34,6 +34,18 @@ class Fila:
     
     def setLast(self, no):
         self.last = no
+    
+    def display(self):
+        if self.empty():
+            print("Erro -> Fila vazia!")
+        else:
+            temp = self.getFirst()
+            while True:
+                if temp == None:
+                    break
+                print(f"{temp.show()}", end=' ')
+                temp = temp.getProx()
+            print()
         
     def empty(self):
         if self.getFirst() == None and self.getLast() == None:
@@ -41,7 +53,8 @@ class Fila:
         return False
         
     def put(self, chave):
-        no = No(chave)
+        valor = int(chave)
+        no = No(valor)
         if self.empty():
             self.setFirst(no)
             self.setLast(no)
@@ -58,3 +71,39 @@ class Fila:
             self.setFirst(temp.getProx())
             temp.setProx(None)
     
+def main():
+    filaIDs = Fila()
+    filaUPs = Fila()
+    quantum = 0
+    maxUP = 0
+    qtdLinhas = int(input())
+    for i in range(qtdLinhas):
+        linha = input().split()
+        if i == 0:
+            quantum = int(linha[0])
+            maxUP = int(linha[1])
+        else:
+            for item in linha:
+                if i == 1:
+                    filaIDs.put(item)
+                else:
+                    filaUPs.put(item)
+    while maxUP > 0:
+        temp = filaUPs.getFirst()
+        up = temp.getChave()
+        for i in range(quantum):
+            if up == 0 or maxUP == 0:
+                break
+            up -= 1
+            maxUP -= 1
+        filaUPs.pop()
+        temp = filaIDs.getFirst()
+        filaIDs.pop()
+        if up > 0:
+            filaUPs.put(up)
+            filaIDs.put(temp.getChave())
+    filaIDs.display()
+    filaUPs.display()
+    
+if __name__ == "__main__":
+    main()
