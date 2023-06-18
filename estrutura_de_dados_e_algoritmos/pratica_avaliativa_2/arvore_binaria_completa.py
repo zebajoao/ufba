b37db4818803449ef1b0663633392699ad791ae8
João Zeba <joaog.zeba@gmail.com>
2023-06-18 17:45:05 -0300

    Métodos da ArvoreBinaria implementados.

--- estrutura_de_dados_e_algoritmos/pratica_avaliativa_2/arvore_binaria_completa.py
+++ estrutura_de_dados_e_algoritmos/pratica_avaliativa_2/arvore_binaria_completa.py
@@ -37,6 +37,65 @@ class No:
         self.dir = no
         
 class ArvoreBinaria:
-    def __init__(self):
+    def __init__(self, profundidade=None):
         self.raiz = None
+        self.profundidade = profundidade
     
+    def getRaiz(self):
+        return self.raiz
+    
+    def setRaiz(self, no):
+        self.raiz = no
+        
+    def getProfundidade(self):
+        return self.profundidade
+    
+    def setProfundidade(self, valor):
+        self.profundidade = valor
+    
+    def add(self, elemento):
+        no = No(elemento)
+        if self.empty():
+            self.setRaiz(no)
+            self.setProfundidade(0)
+            no.setNivel(self.getProfundidade())
+        else:
+            raiz = self.selectRoot(self.getRaiz())
+            if raiz.getEsq() == None:
+                raiz.setEsq(no)
+            else:
+                raiz.setDir(no)
+            self.setProfundidade((raiz.getNivel() + 1))
+            no.setPai(raiz)
+            no.setNivel(self.getProfundidade())
+            
+    def selectRoot(self, raiz):
+        nivelCompleto = self.verifyLevel(raiz.getNivel(), raiz)
+        if nivelCompleto != True:
+            return
+        elif (raiz.getEsq() == None or raiz.getDir() == None):
+            return raiz
+        else:
+            self.selectRoot(raiz.getEsq())
+            self.selectRoot(raiz.getDir())
+            
+        
+    def empty(self):
+        if self.getRaiz() == None:
+            return True
+        return False
+    
+    def newTree():    
+        return ArvoreBinaria()
+    
+    def verifyLevel(self, nivel, raiz, qtdElementos=0):
+        if raiz == None:
+            return
+        elif raiz.getNivel() == nivel:
+            qtdElementos += 1
+            if qtdElementos == (2**(raiz.getNivel())):
+                return True
+            return
+        else:
+            self.verifyLevel(nivel, raiz.getEsq(), qtdElementos)
+            self.verifyLevel(nivel, raiz.getDir(), qtdElementos)
