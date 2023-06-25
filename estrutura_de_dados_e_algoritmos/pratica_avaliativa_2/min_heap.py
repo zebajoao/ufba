eacf4d5051269aecc669b18d3e3c19364f105f89
João Zeba <joaog.zeba@gmail.com>
2023-06-25 13:02:04 -0300

    Finalizado. Lógica dos métodos fixUp() e fixDown() alterada devido à exaustivos sucessíveis problemas com ponteiros.

--- estrutura_de_dados_e_algoritmos/pratica_avaliativa_2/min_heap.py
+++ estrutura_de_dados_e_algoritmos/pratica_avaliativa_2/min_heap.py
@@ -36,14 +36,28 @@ class No:
     def setDir(self, no):
         self.dir = no
     
-    def resetChildren(self):
+    def compareChildren(self):
+        esq = self.getEsq()
+        dir = self.getDir()
+        if esq == None:
+            return dir
+        elif dir == None:
+            return esq
+        elif esq.getChave() < dir.getChave():
+            return esq
+        else:
+            return dir
+    
+    '''
+    def resetChildren(self, inversor=False):
         if self.getDir() == self:
             self.setDir(None)
         if self.getEsq() == self:
             self.setEsq(None)
-        temp = self.getDir()
-        self.setDir(self.getEsq())
-        self.setEsq(temp)
+        if inversor:
+            temp = self.getDir()
+            self.setDir(self.getEsq())
+            self.setEsq(temp)
         
     def setPointers(self, temp, no):
         if self.getDir() == temp:
@@ -51,6 +65,7 @@ class No:
         else:
             self.setEsq(no)
         no.setPai(self)
+    '''
     
     def show(self):
         return f"{self.getChave()}"
@@ -60,11 +75,12 @@ class Heap:
         self.profundidade = 0
         self.qtdElementos = 0
         self.raiz = None
+        self.ultimaFolha = None
         
     def getProfundidade(self):
         return self.profundidade
     
-    def setProfundidade(self, valor):
+    def setProfundidade(self, valor=None):
         self.profundidade = valor
     
     def getQtdElementos(self):
@@ -79,11 +95,24 @@ class Heap:
     def setRaiz(self, no):
         self.raiz = no
         no.setPai(None)
+        
+    def getUltimaFolha(self):
+        return self.ultimaFolha
     
+    def setUltimaFolha(self, no):
+        if no == None:
+            return
+        if no.getNivel() == self.getProfundidade():
+            self.ultimaFolha = no
+        if no.getEsq() != None:
+            self.setUltimaFolha(no.getEsq())
+        self.setUltimaFolha(no.getDir())
+            
     def add(self, chave):
         no = No(int(chave))
         if self.empty():
             self.setRaiz(no)
+            self.setUltimaFolha(no)
         else:
             raiz = self.selectRoot(self.getRaiz())
             if raiz.getEsq() == None:
@@ -94,6 +123,7 @@ class Heap:
             no.setPai(raiz)
         self.setQtdElementos(self.getQtdElementos() + 1)
         no.setNivel(self.getProfundidade())
+        self.setUltimaFolha(no)
         self.fixUp(no)
             
     def empty(self):
@@ -109,36 +139,107 @@ class Heap:
             elif temp.getChave() <= no.getChave():
                 break
             else:
+                aux = no.getChave()
+                no.setChave(temp.getChave())
+                temp.setChave(aux)
+            no = temp
+    
+    def fixDown(self, no):
+        while True:
+            temp = no.compareChildren()
+            if temp == None:
+                break
+            elif temp.getChave() >= no.getChave():
+                break
+            else:
+                aux = no.getChave()
+                no.setChave(temp.getChave())
+                temp.setChave(aux)
+            no = temp
+    
+    '''
+    def fixUp(self, no):
+        while True:
+            temp = no.getPai()
+            if temp == None:
+                break
+            elif temp.getChave() <= no.getChave():
+                break
+            else:
                 temp2 = temp.getPai()
                 if temp2 == None:
                     temp2 = No(no.getChave())
                     temp2.setDir(no.getDir())
                     temp2.setEsq(no.getEsq())
-                    self.relations(temp, no)
+                    self.relationsUp(temp, no)
                     temp.setDir(temp2.getDir())
                     temp.setEsq(temp2.getEsq())
                     self.root(temp, no)
+                    temp.resetChildren()
+                    self.root(temp.getEsq(), temp)
+                    self.root(temp.getDir(), temp)
                     self.setRaiz(no)
                 else:
                     temp2.setPointers(temp, no)
-                    self.relations(temp, no)
+                    self.relationsUp(temp, no)
                     self.root(temp, no)
-                    temp.resetChildren()
+                    temp.resetChildren(inversor=True)
+                    self.root(temp.getEsq(), temp)
+                    self.root(temp.getDir(), temp)
                 temp2 = no.getNivel()
                 no.setNivel(temp.getNivel())
                 temp.setNivel(temp2)
+                self.setUltimaFolha(temp)
                 
-    def inOrder(self, raiz):
-        if raiz == None:
-            return
-        print(f"{raiz.show()}", end=' ')
-        self.inOrder(raiz.getEsq())
-        self.inOrder(raiz.getDir())
+    def fixDown(self, no):
+        while True:
+            temp = no.compareChildren()
+            if temp == None:
+                break
+            elif temp.getChave() > no.getChave():
+                break
+            else:
+                temp2 = no.getPai()
+                if temp2 == None:
+                    temp2 = No(temp.getChave())
+                    temp2.setDir(temp.getDir())
+                    temp2.setEsq(temp.getEsq())
+                    self.relationsDown(no, temp)
+                    no.setDir(temp2.getDir())
+                    no.setEsq(temp2.getEsq())
+                    self.root(no, temp)
+                    no.resetChildren()
+                    self.root(no.getEsq(), no)
+                    self.root(no.getDir(), no)
+                    self.setRaiz(temp)
+                else:
+                    temp2.setPointers(no, temp)
+                    self.relationsDown(no, temp)
+                    self.root(no, temp)
+                    no.resetChildren()
+                    self.root(no.getEsq(), no)
+                    self.root(no.getDir(), no)
+                temp2 = temp.getNivel()
+                temp.setNivel(no.getNivel())
+                no.setNivel(temp2)
+    '''
+    
+    
+    def min(self):
+        return self.getRaiz().getChave()
     
     def newHeap():    
         return Heap()
     
-    def relations(self, temp, no):
+    def preOrder(self, raiz):
+        if raiz == None:
+            return
+        print(f"{raiz.show()}", end=' ')
+        self.preOrder(raiz.getEsq())
+        self.preOrder(raiz.getDir())
+    
+    '''
+    def relationsUp(self, temp, no):
         if temp.getDir() == no:
             temp.setDir(no.getEsq())
             no.setDir(temp)
@@ -151,6 +252,50 @@ class Heap:
             temp.setDir(no.getEsq())
         temp.setPai(no)
     
+    def relationsDown(self, no, temp):
+        if no.getEsq() == temp:
+            no.setEsq(temp.getEsq())
+            temp.setEsq(no.getDir())
+            no.setDir(temp.getDir())
+            temp.setDir(temp.getEsq())
+            temp.setEsq(no)
+        else:
+            no.setDir(temp.getDir())
+            temp.setDir(no.getEsq())
+            no.setEsq(temp.getEsq())
+            temp.setEsq(temp.getDir())
+            temp.setDir(no)
+        no.setPai(temp)
+    '''
+    
+    def remove(self, chave):
+        no = self.search(int(chave), self.getRaiz())
+        folha = self.getUltimaFolha()
+        temp = folha.getPai()
+        if folha == temp.getDir():
+            temp.setDir(None)
+        else:
+            temp.setEsq(None)
+        folha.setPai(no.getPai())
+        folha.setEsq(no.getEsq())
+        folha.setDir(no.getDir())
+        folha.setNivel(no.getNivel())
+        temp = no.getPai()
+        if temp == None:
+            self.setRaiz(folha)
+        elif temp.getEsq() == no:
+            temp.setEsq(folha)
+        else:
+            temp.setDir(folha)
+        no.setPai(None)
+        no.setEsq(None)
+        no.setDir(None)
+        self.setQtdElementos(self.getQtdElementos() - 1)
+        self.updateDepth()
+        self.fixDown(folha)
+        self.setUltimaFolha(self.getRaiz())
+        
+    '''
     def root(self, temp, no):
         if no.getDir() == temp:
             temp2 = no.getEsq()
@@ -158,6 +303,7 @@ class Heap:
             temp2 = no.getDir()
         if temp2 != None:
             temp2.setPai(no)
+    '''
     
     def search(self, chave, raiz):
         if raiz == None:
@@ -167,8 +313,7 @@ class Heap:
         temp = self.search(chave, raiz.getEsq())
         if temp != None:
             return temp
-        else:
-            return self.search(chave, raiz.getDir())
+        return self.search(chave, raiz.getDir())
     
     def selectRoot(self, raiz):
         nivelCompleto = self.verifyLevel(raiz)
@@ -180,14 +325,86 @@ class Heap:
             temp = self.selectRoot(raiz.getEsq())
             if temp != None:
                 return temp
-            else:
-                return self.selectRoot(raiz.getDir())
+            return self.selectRoot(raiz.getDir())
+    
+    def updateDepth(self):
+        temp = self.getRaiz()
+        while temp.getEsq() != None:
+            temp = temp.getEsq()
+        self.setProfundidade(temp.getNivel())
     
     def verifyLevel(self, raiz):
         if self.getQtdElementos() >= ((2**(raiz.getNivel() + 1)) - 1):
             return True
         else:
             return False
+        
+class Vetor:
+    def __init__(self, tam):
+        self.inicio = None
+        self.fim = None
+        self.tamanho = tam
+        self.qtdElementos = 0
+    
+    def getInicio(self):
+        return self.inicio
+    
+    def setInicio(self, no):
+        self.inicio = no
+        
+    def getFim(self):
+        return self.fim
+    
+    def setFim(self, no):
+        self.fim = no
+        
+    def getTam(self):
+        return self.tamanho
+    
+    def setTam(self, valor):
+        self.tamanho = valor
+    
+    def getQtdElementos(self):
+        return self.qtdElementos
+    
+    def setQtdElementos(self, valor):
+        self.qtdElementos = valor
+        
+    def append(self, chave):
+        no = No(int(chave))
+        if self.empty():
+            self.setInicio(no)
+            self.setFim(no)
+            self.setQtdElementos(self.getQtdElementos() + 1)
+        elif self.full():
+            print("Erro -> Vetor cheio!")
+        else:
+            temp = self.getFim()
+            temp.setDir(no)
+            no.setEsq(temp)
+            self.setFim(no)
+            self.setQtdElementos(self.getQtdElementos() + 1)
+    
+    def display(self):
+        if self.empty():
+            print("Erro -> Vetor vazio!")
+        else:
+            temp = self.getInicio()
+            while True:
+                if temp == None:
+                    break
+                print(temp.show(), end=' ')
+                temp = temp.getDir()
+            
+    def empty(self):
+        if self.getQtdElementos() == 0:
+            return True
+        return False
+    
+    def full(self):
+        if self.getQtdElementos() == self.getTam():
+            return True
+        return False
         
 def main():
     qtdLinhas = int(input())
@@ -196,7 +413,14 @@ def main():
     minHeap = Heap.newHeap()
     for elemento in vetorDeEntrada:
         minHeap.add(elemento)
-    minHeap.inOrder(minHeap.getRaiz())
+    vetor = Vetor(numIteracoes)
+    for i in range(numIteracoes):
+        elemento = minHeap.min()
+        minHeap.remove(elemento)
+        vetor.append(elemento)
+        minHeap.preOrder(minHeap.getRaiz())
+        print()
+    vetor.display()
     
 if __name__=="__main__":
     main()
